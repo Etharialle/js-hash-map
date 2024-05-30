386b3e436e7f715e2167b47b8aba79ee3b91d7b9
etharialle <christopher.m.adams2@gmail.com>
2024-05-29 17:32:06 -0700

    feat: add hash fucntion

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-if (index < 0 || index >= buckets.length) {
-    throw new Error("Trying to access index out of bound");
-  }
+//if (index < 0 || index >= buckets.length) {
+//    throw new Error("Trying to access index out of bound");
+//}
+
+  class HashMap {
+    constructor () {
+
+    }
+    hash(key) {
+        let hashCode = 0;
+      
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+        return hashCode;
+    }
+    set(key, value) {
+        
+    }
+}
+
+
